63563236365de44e70510f6ea734de1e056d0d69
Justin Hardcastle <ScottCodes@Justins-MacBook-Pro.local>
2016-07-15 09:21:06 -0400

    Update

---
+++ js/script_practice.js
@@ -0,0 +1,20 @@
+ document.getElementById("nav").innerHTML =
+ '<ul>'+
+  '<a href="practice.html" id="nav">Html/CSS</a> | '+
+  '<a href="practice_site/js.html" id="nav">JavaScript</a> | '+
+  '<a href="practice_site/jq.html" id="nav">JQuery</a> | '+
+  '<a href="practice_site/angular.html" id="nav">AngularJS</a> | '+
+  '<a href="practice_site/line.html" id="nav">Command Line</a> | '+
+  '<a href="practice_site/ruby.html" id="nav">Ruby</a> | '+
+  '<a href="practice_site/rubyrails.html" id="nav">Ruby on Rails</a> | '+
+  '<a href="practice_site/php.html" id="nav">PHP</a> | '+
+  '<a href="practice_site/sql.html" id="nav">SQL</a> | '+
+  '<a href="practice_site/railsauth.html" id="nav">Rails Authentication</a> | '+
+  '<a href="practice_site/learn.html" id="nav">Learn Git</a>'+
+  
+ '</ul>';
+ 
+ 
+ 
+ 
+ 
